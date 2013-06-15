e99aeadd7ab34c1af8cc3ccec3364f9c664a6f23
Daniel Cohen Gindi <danielgindi@gmail.com>
2013-06-15 20:34:07 +0300

    Detect if current locale is rtl

---
+++ Additions/NSLocale+RtlDetection.h
@@ -0,0 +1,17 @@
+//
+//  NSLocale+RtlDetection.h
+//  Additions
+//
+//  Created by Daniel Cohen Gindi on 6/15/13.
+//  Copyright (c) 2013 Daniel Cohen Gindi. All rights reserved.
+//
+//  https://github.com/danielgindi/drunken-danger-zone
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSLocale (RtlDetection)
+
++ (BOOL)isActiveLocaleRtl;
+
+@end
