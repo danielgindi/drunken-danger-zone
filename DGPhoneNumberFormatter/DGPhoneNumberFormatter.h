9a0163e932da60962d448b332c2f317506f0616c
Daniel Cohen Gindi <danielgindi@gmail.com>
2013-12-15 18:12:13 +0200

    clarification

--- DGPhoneNumberFormatter/DGPhoneNumberFormatter.h
+++ DGPhoneNumberFormatter/DGPhoneNumberFormatter.h
@@ -10,7 +10,8 @@
 //
 //  This class is an NSFormatter that can be used like any other Cocoa formatter,
 //    to format phone numbers in a manner similar to the iPhone's Phone app.
-//
+//  Clarification: This is currently design for formatting number when calling from inside Israel, to either national or international number.
+//    Other countries may be supported in the future.
 
 #import <Foundation/Foundation.h>
 
